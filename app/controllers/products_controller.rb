461625005833996cbf67f541090ee9dc4569c1f9
Luis Emanuel Banega <emabanega@hotmail.com>
2024-12-04 23:26:16 -0300

    Genero controlador de Productos

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to products_path, notice: '¡Producto creado exitosamente!'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+
+        if @product.update(product_params)
+            redirect_to products_path, notice: '¡Producto editado exitosamente!'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+
+        redirect_to products_path, notice: '¡Producto eliminado exitosamente!', status: :see_other
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :description, :unit_price, :stock, :category, :size, :color)
+    end
+end
